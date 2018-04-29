7bdd8036bed202a5067f045f3c30a4dd77dba9d1
Radislav Berkovich <radislav.berkovich@hpe.com>
2018-04-29 15:27:24 +0300

    user story #612030 : Octane UFT integration with Bamboo - POC. Exposing test fields in test results

--- integrations-dto/src/main/java/com/hp/octane/integrations/dto/tests/impl/DTOTestsProvider.java
+++ integrations-dto/src/main/java/com/hp/octane/integrations/dto/tests/impl/DTOTestsProvider.java
@@ -19,10 +19,7 @@ package com.hp.octane.integrations.dto.tests.impl;
 import com.hp.octane.integrations.dto.DTOBase;
 import com.hp.octane.integrations.dto.DTOFactory;
 import com.hp.octane.integrations.dto.DTOInternalProviderBase;
-import com.hp.octane.integrations.dto.tests.BuildContext;
-import com.hp.octane.integrations.dto.tests.TestRun;
-import com.hp.octane.integrations.dto.tests.TestRunError;
-import com.hp.octane.integrations.dto.tests.TestsResult;
+import com.hp.octane.integrations.dto.tests.*;
 
 /**
  * Octane oriented test result DTOs definitions provider
@@ -34,11 +31,13 @@ public final class DTOTestsProvider extends DTOInternalProviderBase {
 		dtoPairs.put(BuildContext.class, BuildContextImpl.class);
 		dtoPairs.put(TestRunError.class, TestRunErrorImpl.class);
 		dtoPairs.put(TestRun.class, TestRunImpl.class);
+		dtoPairs.put(TestField.class, TestFieldImpl.class);
 		dtoPairs.put(TestsResult.class, TestsResultImpl.class);
 
 		xmlAbles.add(BuildContextImpl.class);
 		xmlAbles.add(TestRunErrorImpl.class);
 		xmlAbles.add(TestRunImpl.class);
+		xmlAbles.add(TestFieldImpl.class);
 		xmlAbles.add(TestsResultImpl.class);
 	}
 
