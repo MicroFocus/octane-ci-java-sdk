01fa6277d4d0a43299cbe2c00cc51132b15f0aec
gullery <yuri.guller@hpe.com>
2018-06-11 13:13:01 +0300

    tech: improving the interim APIs of DTO module, adding queueing and log dispatching capabilities to an SDK

--- integrations-sdk/src/main/java/com/hp/octane/integrations/services/bridge/BridgeServiceImpl.java
+++ integrations-sdk/src/main/java/com/hp/octane/integrations/services/bridge/BridgeServiceImpl.java
@@ -31,7 +31,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
-import java.net.ConnectException;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
