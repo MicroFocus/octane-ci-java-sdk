b31b4094a706f4603b2bd159355b85638d17035c
Radislav Berkovich <radislav.berkovich@hpe.com>
2018-06-12 11:09:02 +0300

    user story #651006 : [Tech] [UFT integration]Move common logic related to UFT integration to sdk

---
+++ integrations-sdk/src/main/java/com/hp/octane/integrations/util/Constants.java
@@ -0,0 +1,9 @@
+package com.hp.octane.integrations.util;
+
+public class Constants {
+
+    public static class FileSystem{
+        public static final String WINDOWS_PATH_SPLITTER = "\\";
+        public static final String LINUX_PATH_SPLITTER = "/";
+    }
+}
