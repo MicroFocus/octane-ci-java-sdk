8cde481a9943a5f78e8eb1f7ace6f29243ade7a4
Radislav Berkovich <radislav.berkovich@hpe.com>
2018-07-09 15:19:49 +0300

    user story #648105 [bamboo UFT integration] as a user I want to discover UFT tests using Bamboo CI Server - handling non-english characters

--- integrations-dto/src/main/java/com/hp/octane/integrations/dto/connectivity/impl/OctaneRequestImpl.java
+++ integrations-dto/src/main/java/com/hp/octane/integrations/dto/connectivity/impl/OctaneRequestImpl.java
@@ -26,6 +26,7 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 /**
@@ -89,7 +90,7 @@ class OctaneRequestImpl implements OctaneRequest {
 
 	@JsonProperty
 	public OctaneRequest setBody(String body) {
-		this.body = new ByteArrayInputStream(body.getBytes());
+		this.body = new ByteArrayInputStream(body.getBytes(StandardCharsets.UTF_8));
 		return this;
 	}
 }
