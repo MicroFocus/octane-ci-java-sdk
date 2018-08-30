062f8eba0e9737485f2d4fe45d894a09f6d70675
gullery <yuri.guller@hpe.com>
2018-08-30 11:11:13 +0300

    us #710018: enhancing events queue handling to accomodate with Octane's server sane downtime with minimum data loss

--- integrations-sdk/src/main/java/com/hp/octane/integrations/OctaneSDK.java
+++ integrations-sdk/src/main/java/com/hp/octane/integrations/OctaneSDK.java
@@ -34,8 +34,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Properties;
 
 /**
