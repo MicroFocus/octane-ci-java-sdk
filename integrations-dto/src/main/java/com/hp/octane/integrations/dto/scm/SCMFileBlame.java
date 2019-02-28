3f9b9a1b35a85fbf940a0e4bc9bf5021085b9663
eli emanuel <eemanuel@microfocus.com>
2019-02-28 11:27:59 +0200

    tech: optimizing imports

--- integrations-dto/src/main/java/com/hp/octane/integrations/dto/scm/SCMFileBlame.java
+++ integrations-dto/src/main/java/com/hp/octane/integrations/dto/scm/SCMFileBlame.java
@@ -1,10 +1,6 @@
 package com.hp.octane.integrations.dto.scm;
 
-import com.hp.octane.integrations.dto.scm.impl.LineRange;
 import com.hp.octane.integrations.dto.scm.impl.RevisionsMap;
-
-import java.util.List;
-import java.util.Map;
 
 public interface SCMFileBlame {
 
