8c2f72006df021c49990ae7906faf50b84c9e7a1
emanueli <eemanuel@microfocus.com>
2019-04-15 17:37:22 +0300

    defect #828240: jenkins plugin - commits are not injected to octane in jobs with remote worker

--- integrations-dto/src/main/java/com/hp/octane/integrations/dto/scm/SCMFileBlame.java
+++ integrations-dto/src/main/java/com/hp/octane/integrations/dto/scm/SCMFileBlame.java
@@ -1,8 +1,9 @@
 package com.hp.octane.integrations.dto.scm;
 
+import com.hp.octane.integrations.dto.DTOBase;
 import com.hp.octane.integrations.dto.scm.impl.RevisionsMap;
 
-public interface SCMFileBlame {
+public interface SCMFileBlame extends DTOBase {
 
     String getPath();
     RevisionsMap getRevisionsMap();
