f04e2fba4ac36ac4f00bfa733c85ac7ecb3b9d5a
Radislav Berkovich <radislav.berkovich@microfocus.com>
2021-07-25 15:57:39 +0300

    mbt : action result extraction

--- integrations-sdk/src/main/java/com/hp/octane/integrations/uft/ufttestresults/schema/UftResultData.java
+++ integrations-sdk/src/main/java/com/hp/octane/integrations/uft/ufttestresults/schema/UftResultData.java
@@ -2,17 +2,19 @@ package com.hp.octane.integrations.uft.ufttestresults.schema;
 
 import java.util.List;
 
-public class UftErrorData {
+public class UftResultData {
     private List<String> parents;
     private String type;
     private String result;
     private String message;
+    private long duration;
 
-    public UftErrorData(List<String> parents, String type, String result, String message) {
+    public UftResultData(List<String> parents, String type, String result, String message, long duration) {
         this.parents = parents;
         this.type = type;
         this.result = result;
         this.message = message;
+        this.duration = duration;
     }
 
     public List<String> getParents() {
@@ -29,5 +31,13 @@ public class UftErrorData {
 
     public String getResult() {
         return result;
+    }
+
+    public String toString() {
+        return String.join("/", parents) + ":" + result + (message != null ? "," + message : "");
+    }
+
+    public long getDuration() {
+        return duration;
     }
 }
