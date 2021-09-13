3c4f36a45840690d62637633ec1bae925f3339c5
Radislav Berkovich <radislav.berkovich@microfocus.com>
2021-09-13 16:19:31 +0300

    user story #1354266 : Allow to create and run suite runs via a CI JOB - remove favorite mode

--- integrations-sdk/src/main/java/com/hp/octane/integrations/octaneExecution/ExecutionMode.java
+++ integrations-sdk/src/main/java/com/hp/octane/integrations/octaneExecution/ExecutionMode.java
@@ -25,8 +25,8 @@ import java.io.Serializable;
 public enum ExecutionMode implements Serializable {
 
     SUITE_RUNS_IN_OCTANE("suite_runs_in_octane", "Execute suite runs in ALM Octane"),
-    SUITE_IN_CI("suites_in_ci", "Get tests from suites and trigger execution jobs"),
-    FAVORITES_IN_CI("favorites_in_ci", "Get tests from favorites and trigger execution jobs");
+    SUITE_IN_CI("suites_in_ci", "Get tests from suites and trigger execution jobs");
+    //FAVORITES_IN_CI("favorites_in_ci", "Get tests from favorites and trigger execution jobs");
 
     private final String value;
     private final String desc;
